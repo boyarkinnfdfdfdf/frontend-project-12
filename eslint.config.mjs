eb2881ddd3fb7f125d0519b8e97e32f8c703b851
Dimitry Plehanov <mitya-plekhanov@mail.ru>
2025-05-26 11:19:32 +0300

    32

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,9 @@
-import js from '@eslint/js';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
+  js.configs.recommended,
   {
-    ignores: ['node_modules', 'dist', 'build'],
-  },
-  {
-    files: ['/*/*.js', '/*/*.jsx'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        React: 'writable',
-        localStorage: 'readonly',
-        window: 'readonly',
-        document: 'readonly',
-        navigator: 'readonly',
-        fetch: 'readonly',
-        console: 'readonly',
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': reactHooksPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      'quote-props': ['error', 'as-needed'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'comma-spacing': ['error', { before: false, after: true }],
-      'no-unused-vars': 'warn',
-      'no-undef': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-];
+    languageOptions: { globals: globals.node }
+  }
+]
